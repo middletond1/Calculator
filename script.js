d34227631c9690afaa86451cf1a4b1d24858fb31
David Middleton <middleton.davida@gmail.com>
2021-05-23 15:11:23 -0400

    added variable clickedNumber to getNumberFromButton function. Allows getNumberFromButton function to return a single number.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const numberButtons = document.querySelectorAll('.number');
 const buttonArea = document.querySelector('#buttons');
+const numberInput = document.querySelector('#numberinput');
 
 function getNumberFromButton(button) {
+    let clickedNumber = '';
     Array.from(numberButtons).map(numberButton => {
         if (button.target.textContent === numberButton.textContent) {
-            return numberButton.textContent;
-        }
+            clickedNumber = numberButton.textContent;
+        };
     });
+    return clickedNumber;
 }
 
-buttonArea.addEventListener('click', getNumberFromButton);
+
+function writeNumberToNumberInput(button) {
+    console.log(getNumberFromButton(button));
+}
+
+buttonArea.addEventListener('click', writeNumberToNumberInput);
