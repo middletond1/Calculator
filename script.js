b9d7431df6ea046678520c7abc000e720f36298e
David Middleton <middleton.davida@gmail.com>
2021-05-23 13:50:05 -0400

    reordered numbers in html, added number button selector for js

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('.number');
+
+function getNumberFromButton() {
+    // numberButtons.map(numberButton => console.log(numberButton));
+    console.log(numberButtons)
+}
+
+getNumberFromButton();
