31961388db8f2573e73d794eba138e0db951a107
David Middleton <middleton.davida@gmail.com>
2021-05-28 19:07:49 -0400

    added equals function and first and second operand concat

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
 const numberButtons = document.querySelectorAll('.number');
-const buttonArea = document.querySelector('#buttons');
+const operatorButtons = document.querySelectorAll('.operator');
 const numberInput = document.querySelector('#numberinput');
-const additionButton = document.querySelector('#addition');
-const subtractionButton = document.querySelector('#subtraction');
-const multiplicationButton = document.querySelector('#multiplication');
-const divisionButton = document.querySelector('#division');
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
 let solution = '';
 
 
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => {
+        const number = numberButton.textContent;
+
+        if (!operator) {
+            firstOperand += number;
+        }
+        if(operator) {
+            secondOperand += number;
+        }
+
+        console.log('firstOperand', firstOperand);
+
+        console.log('secondOperand', secondOperand);
+
+    })
+})
+
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', (event) => {
+        const selectedOperator = event.target.textContent;
+
+        if (firstOperand) {
+            operator = selectedOperator;
+        }
+
+        console.log(operator);
+    })
+})
+
+document.querySelector('#equals').addEventListener('click', () => {
+    firstNumber = parseFloat(firstOperand);
+    secondNumber = parseFloat(secondOperand);
+    if (firstOperand && secondOperand && operator) {
+        if (operator === '+') {
+            solution = firstNumber + secondNumber;
+        };
+        if (operator === '-') {
+            solution = firstNumber - secondNumber;
+        };
+        if (operator === 'X') {
+            solution = firstNumber * secondNumber;
+        };
+        if (operator === '÷') {
+            solution = firstNumber / secondNumber;
+        };
+    }
+    console.log(solution);
+})
